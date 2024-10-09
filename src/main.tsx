67d473278ab469fed556fe7828a4a3626cf0d57a
Yaver-496 <abbasovmamed8@gmail.com>
2024-10-09 17:49:19 +0400

    init
    dfv

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-const manifestUrl = 'https://IAVARABBASOV.github.io/my-twa/tonconnect-manifest.json';
+const manifestUrl = 'https://yaver-496.github.io/my-twa/tonconnect-manifest.json';
 
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
